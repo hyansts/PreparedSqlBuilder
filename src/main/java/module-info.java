44e71b9a621f018f17edc6acce41cf089c27a708
Hyan Santos <hyansts@gmail.com>
2025-05-01 15:14:34 -0300

    🏗️ Add module-info file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.github.hyansts.preparedsqlbuilder {
+	exports com.github.hyansts.preparedsqlbuilder.query;
+	exports com.github.hyansts.preparedsqlbuilder.query.impl;
+	exports com.github.hyansts.preparedsqlbuilder.db;
+	exports com.github.hyansts.preparedsqlbuilder.db.impl;
+	exports com.github.hyansts.preparedsqlbuilder.sql;
+}
